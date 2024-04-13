c4911d05483bfd8072d5311816a9b0b4cc4f108e
haaplaus <haaplausgames@gmail.com>
2024-04-13 12:04:44 +0200

    new70

--- commands/utility/nigg.js
+++ commands/utility/nigg.js
@@ -1,10 +1,11 @@
+
 const {SlashCommandBuilder} = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('ping')
-        .setDescription('Pong!'),
+        .setName('nigg')
+        .setDescription('nog!'),
     async execute(interaction){
-        await interaction.reply('Pong!');
+        await interaction.reply('nog!');
     }
 };
